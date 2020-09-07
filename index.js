985a6c6db5685479cd0555bef22922a512440425
Xiaoyi Liu <lxy878@gmail.com>
2020-09-07 08:09:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,13 @@ class Dog{
     return `${this.name} says woof!`;
   }
 }
+
+class Bird{
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+  }
+  speak(){
+    return (this.sex === 'male') ? `It's me! ${this.name}, the parrot!`:`${this.name} says squawk!`
+  }
+}
